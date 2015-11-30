2f25a104740ba4579f5380210a6bc87a9f92ecce
Adrian <adrian@mxlinux.org>
2015-11-30 10:09:56 -0500

    change app icon location

--- main.cpp
+++ main.cpp
@@ -34,7 +34,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    a.setWindowIcon(QIcon("/usr/share/pixmaps/mx/mx-welcome.png"));
+    a.setWindowIcon(QIcon("/usr/share/pixmaps/mx-welcome.png"));
 
     QTranslator qtTran;
     qtTran.load(QString("qt_") + QLocale::system().name());
