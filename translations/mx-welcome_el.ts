67d77ebfd2344f554c3db4ca296dd0eb16fe5b91
oldgizahub <geoffgigg@hotmail.com>
2017-12-11 21:24:03 -0500

    updated de el es fr lt nl translations for mx-welcome

--- translations/mx-welcome_el.ts
+++ translations/mx-welcome_el.ts
@@ -1,10 +1,8 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="el">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="el" version="2.1">
 <context>
     <name>QApplication</name>
     <message>
-        <location filename="../main.cpp" line="55"/>
+        <location filename="main.cpp" line="55"/>
         <source>You must run this program as normal user.</source>
         <translation>Πρέπει να τρέξετε αυτή την εφαρμογή ως κανονικός χρήστης</translation>
     </message>
@@ -12,147 +10,147 @@
 <context>
     <name>mxwelcome</name>
     <message>
-        <location filename="../mxwelcome.ui" line="14"/>
-        <location filename="../mxwelcome.cpp" line="51"/>
-        <location filename="../mxwelcome.cpp" line="108"/>
-        <location filename="../mxwelcome.cpp" line="115"/>
+        <location filename="mxwelcome.ui" line="14"/>
+        <location filename="mxwelcome.cpp" line="51"/>
+        <location filename="mxwelcome.cpp" line="108"/>
+        <location filename="mxwelcome.cpp" line="115"/>
         <source>MX Welcome</source>
         <translation>MX Welcome</translation>
     </message>
     <message>
-        <location filename="../mxwelcome.ui" line="55"/>
+        <location filename="mxwelcome.ui" line="55"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:12pt; font-weight:600;&quot;&gt;Welcome to MX Linux, a fast, friendly, and stable OS loaded with amazing tools and supported by a great community. &lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt; font-weight:600;&quot;&gt;Καλώς ήρθατε στο MX Linux - ένα φιλικό, γρήγορο, σταθερό λειτουργικό σύστημα- φορτωμένο με καταπληκτικά εργαλεία και υποστηρίζεται από μια μεγάλη κοινότητα. &lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../mxwelcome.ui" line="113"/>
+        <location filename="mxwelcome.ui" line="113"/>
         <source>Users Manual</source>
         <translation>Εγχειρίδιο</translation>
     </message>
     <message>
-        <location filename="../mxwelcome.ui" line="164"/>
+        <location filename="mxwelcome.ui" line="164"/>
         <source>Forums</source>
         <translation>Φόρουμ</translation>
     </message>
     <message>
-        <location filename="../mxwelcome.ui" line="215"/>
+        <location filename="mxwelcome.ui" line="215"/>
         <source>Videos</source>
         <translation>βίντεο</translation>
     </message>
     <message>
-        <location filename="../mxwelcome.ui" line="247"/>
+        <location filename="mxwelcome.ui" line="247"/>
         <source>Wiki</source>
         <translation>Wiki</translation>
     </message>
     <message>
-        <location filename="../mxwelcome.ui" line="331"/>
+        <location filename="mxwelcome.ui" line="331"/>
         <source>Tools</source>
         <translation>Εργαλεία </translation>
     </message>
     <message>
-        <location filename="../mxwelcome.ui" line="370"/>
+        <location filename="mxwelcome.ui" line="370"/>
         <source>Contribute</source>
         <translation>Συνεισφέρω</translation>
     </message>
     <message>
-        <location filename="../mxwelcome.ui" line="415"/>
+        <location filename="mxwelcome.ui" line="415"/>
         <source>Tweak</source>
-        <translation type="unfinished"></translation>
+        <translation>Τσιμπιά</translation>
     </message>
     <message>
-        <location filename="../mxwelcome.ui" line="441"/>
+        <location filename="mxwelcome.ui" line="441"/>
         <source>Popular Apps</source>
         <translation>δημοφιλείς εφαρμογές</translation>
     </message>
     <message>
-        <location filename="../mxwelcome.ui" line="464"/>
+        <location filename="mxwelcome.ui" line="464"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:14pt; font-weight:600;&quot;&gt;MX-17 &amp;quot;Horizon&amp;quot;&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:14pt; font-weight:600;&quot;&gt;MX-17 &amp;quot;Horizon&amp;quot;&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../mxwelcome.ui" line="575"/>
+        <location filename="mxwelcome.ui" line="575"/>
         <source>Login Info</source>
         <translation>Πληροφορίες Login</translation>
     </message>
     <message>
-        <location filename="../mxwelcome.ui" line="595"/>
+        <location filename="mxwelcome.ui" line="595"/>
         <source>Show this dialog at start up</source>
         <translation>Εμφάνιση αυτού του διαλόγου κατά την εκκίνηση</translation>
     </message>
     <message>
-        <location filename="../mxwelcome.ui" line="653"/>
+        <location filename="mxwelcome.ui" line="653"/>
         <source>About this application</source>
         <translation>Περί εφαρμογής.</translation>
     </message>
     <message>
-        <location filename="../mxwelcome.ui" line="656"/>
+        <location filename="mxwelcome.ui" line="656"/>
         <source>About...</source>
         <translation>Περί</translation>
     </message>
     <message>
-        <location filename="../mxwelcome.ui" line="664"/>
+        <location filename="mxwelcome.ui" line="663"/>
         <source>Alt+B</source>
         <translation>Alt+B</translation>
     </message>
     <message>
-        <location filename="../mxwelcome.ui" line="718"/>
+        <location filename="mxwelcome.ui" line="717"/>
         <source>Quit application</source>
         <translation>Κλείστε την εφαρμογή </translation>
     </message>
     <message>
-        <location filename="../mxwelcome.ui" line="721"/>
+        <location filename="mxwelcome.ui" line="720"/>
         <source>Close</source>
         <translation>Κλείσιμο</translation>
     </message>
     <message>
-        <location filename="../mxwelcome.ui" line="729"/>
+        <location filename="mxwelcome.ui" line="727"/>
         <source>Alt+N</source>
         <translation>Alt+N</translation>
     </message>
     <message>
-        <location filename="../mxwelcome.cpp" line="107"/>
+        <location filename="mxwelcome.cpp" line="107"/>
         <source>About MX Welcome</source>
         <translation>Περί MX Welcome</translation>
     </message>
     <message>
-        <location filename="../mxwelcome.cpp" line="108"/>
+        <location filename="mxwelcome.cpp" line="108"/>
         <source>Version: </source>
         <translation>Έκδοση:</translation>
     </message>
     <message>
-        <location filename="../mxwelcome.cpp" line="109"/>
+        <location filename="mxwelcome.cpp" line="109"/>
         <source>Program for displaying a welcome screen in MX Linux</source>
         <translation>Πρόγραμμα για την εμφάνιση μια ευπρόσδεκτη οθόνη MX Linux</translation>
     </message>
     <message>
-        <location filename="../mxwelcome.cpp" line="111"/>
+        <location filename="mxwelcome.cpp" line="111"/>
         <source>Copyright (c) MX Linux</source>
         <translation>Copyright (c)  MX Linux </translation>
     </message>
     <message>
-        <location filename="../mxwelcome.cpp" line="112"/>
+        <location filename="mxwelcome.cpp" line="112"/>
         <source>License</source>
         <translation>Άδεια</translation>
     </message>
     <message>
-        <location filename="../mxwelcome.cpp" line="113"/>
+        <location filename="mxwelcome.cpp" line="113"/>
         <source>Cancel</source>
         <translation>Ακύρωση</translation>
     </message>
     <message>
-        <location filename="../mxwelcome.cpp" line="181"/>
+        <location filename="mxwelcome.cpp" line="181"/>
         <source>Live Login Info</source>
-        <translation type="unfinished"></translation>
+        <translation>Πληροφορίες Login</translation>
     </message>
     <message>
-        <location filename="../mxwelcome.cpp" line="182"/>
+        <location filename="mxwelcome.cpp" line="182"/>
         <source>Default username:  &lt;b&gt;demo&lt;/b&gt; &lt;/p&gt; &lt;p align=&quot;center&quot;&gt;Default demo password:  &lt;b&gt;demo&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Προεπιλεγμένο όνομα χρήστη: &lt;b&gt;demo&lt;/b&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;Προεπιλεγμένος κωδικός πρόσβασης του demo&lt;b&gt;demo&lt;/b&gt;</translation>
     </message>
     <message>
-        <location filename="../mxwelcome.cpp" line="183"/>
+        <location filename="mxwelcome.cpp" line="183"/>
         <source>&lt;b&gt;Root&lt;/b&gt; Password:  &lt;b&gt;root&lt;/b&gt;&lt;/p&gt; </source>
-        <translation type="unfinished"></translation>
+        <translation>Κωδικός πρόσβασης &lt;b&gt;root&lt;/b&gt;: &lt;b&gt;root&lt;/b&gt;&lt;/p&gt;</translation>
     </message>
 </context>
 </TS>
