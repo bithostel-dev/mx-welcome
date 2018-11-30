729ebb384580f8b58d6338da48d0266e6fd4cd08
Adrian <adrian@mxlinux.org>
2018-11-30 15:22:25 -0500

    new build with new translations

---
+++ mx-welcome_18.11.dsc
@@ -0,0 +1,32 @@
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA256
+
+Format: 3.0 (native)
+Source: mx-welcome
+Binary: mx-welcome
+Architecture: any
+Version: 18.11
+Maintainer: Adrian <adrian@mxlinux.org>
+Standards-Version: 3.9.8
+Vcs-Git: git://github.com/AdrianTM/mx-welcome
+Build-Depends: qt5-default, qttools5-dev-tools, debhelper (>= 9)
+Package-List:
+ mx-welcome deb admin optional arch=any
+Checksums-Sha1:
+ 7c07a128f7701915863c30bfa3fc91e2c1bf98f3 68512 mx-welcome_18.11.tar.xz
+Checksums-Sha256:
+ 7cb956389a894702dffcea6916094608e5b9a507fabce5c010e7b0a62a7c506c 68512 mx-welcome_18.11.tar.xz
+Files:
+ 3521083d0ae279f0712addeee70a8998 68512 mx-welcome_18.11.tar.xz
+
+-----BEGIN PGP SIGNATURE-----
+
+iQFHBAEBCAAxFiEE8ndToY6S45N+YzXncJOMeAZ57pgFAlwBm60THGFkcmlhbkBt
+eGxpbnV4Lm9yZwAKCRBwk4x4BnnumMnzB/4zahXMjLYs2WI6MOZJztU8fz0VmwKq
+KR1HKkRhs+dGhmW16tX5ukqHlQ6EFx9ggYARiOIJUb2Q/zyA7V49uy2Uz27t9OfV
+OIqB6XdYOtOVX39EOqSglpiiJKvQ8yWgb9j4ywrL0Nad4+BUXrrIF8SC+YIdMdnA
+Pto/RH8cDblIaFd3Is8XG2oRT4dSrO2RarI1RDohyZfFF5dP++hbS/fqAA+F8Lgv
+HJvjWCqZIhGiOQzs6B0tolreO73tb+eE4XA0aquTq2xyWxF3gwtAHsYAdZD1UP6h
+OLWW4bTdDHeLRXJAkKy0GvpHkJNrnti9ZniPwETxeuU82cMC8NOVlqkx
+=dp8W
+-----END PGP SIGNATURE-----
--- translations/mx-welcome_is.ts
+++ translations/mx-welcome_is.ts
@@ -1,166 +1,168 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="is" version="2.1">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="is">
 <context>
     <name>QApplication</name>
     <message>
-        <location filename="main.cpp" line="55"/>
+        <location filename="../main.cpp" line="55"/>
         <source>You must run this program as normal user.</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>mxwelcome</name>
     <message>
-        <location filename="mxwelcome.ui" line="14"/>
-        <location filename="mxwelcome.cpp" line="54"/>
-        <location filename="mxwelcome.cpp" line="95"/>
-        <location filename="mxwelcome.cpp" line="107"/>
+        <location filename="../mxwelcome.ui" line="14"/>
+        <location filename="../mxwelcome.cpp" line="54"/>
+        <location filename="../mxwelcome.cpp" line="95"/>
+        <location filename="../mxwelcome.cpp" line="107"/>
         <source>MX Welcome</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="51"/>
+        <location filename="../mxwelcome.ui" line="51"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:12pt; font-weight:600;&quot;&gt;Welcome to MX Linux, a fast, friendly, and stable OS loaded with amazing tools and supported by a great community. &lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="109"/>
+        <location filename="../mxwelcome.ui" line="109"/>
         <source>Users Manual</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="160"/>
+        <location filename="../mxwelcome.ui" line="160"/>
         <source>Forums</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="211"/>
+        <location filename="../mxwelcome.ui" line="211"/>
         <source>Videos</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="243"/>
+        <location filename="../mxwelcome.ui" line="243"/>
         <source>Wiki</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="327"/>
+        <location filename="../mxwelcome.ui" line="327"/>
         <source>Tools</source>
         <translation>Kerfistól</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="366"/>
+        <location filename="../mxwelcome.ui" line="366"/>
         <source>Contribute</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="411"/>
+        <location filename="../mxwelcome.ui" line="411"/>
         <source>Tweak (Panel, etc...)</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="437"/>
+        <location filename="../mxwelcome.ui" line="437"/>
         <source>Popular Apps</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="460"/>
+        <location filename="../mxwelcome.ui" line="460"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:14pt; font-weight:600;&quot;&gt;MX-17 &amp;quot;Horizon&amp;quot;&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="571"/>
+        <location filename="../mxwelcome.ui" line="571"/>
         <source>Login Info</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="591"/>
+        <location filename="../mxwelcome.ui" line="591"/>
         <source>Show this dialog at start up</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="649"/>
+        <location filename="../mxwelcome.ui" line="649"/>
         <source>About this application</source>
         <translation>Um þetta forrit</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="652"/>
+        <location filename="../mxwelcome.ui" line="652"/>
         <source>About...</source>
         <translation>Um hugbúnaðinn...</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="659"/>
+        <location filename="../mxwelcome.ui" line="659"/>
         <source>Alt+B</source>
         <translation>Alt+B</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="713"/>
+        <location filename="../mxwelcome.ui" line="713"/>
         <source>Quit application</source>
         <translation>Hætta í forriti</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="716"/>
+        <location filename="../mxwelcome.ui" line="716"/>
         <source>Close</source>
         <translation>Hætta</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="723"/>
+        <location filename="../mxwelcome.ui" line="723"/>
         <source>Alt+N</source>
         <translation>Alt+N</translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="94"/>
+        <location filename="../mxwelcome.cpp" line="94"/>
         <source>About MX Welcome</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="95"/>
+        <location filename="../mxwelcome.cpp" line="95"/>
         <source>Version: </source>
         <translation>Útgáfa:</translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="96"/>
+        <location filename="../mxwelcome.cpp" line="96"/>
         <source>Program for displaying a welcome screen in MX Linux</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="98"/>
+        <location filename="../mxwelcome.cpp" line="98"/>
         <source>Copyright (c) MX Linux</source>
         <translation>Höfundarréttur (c) MX Linux</translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="99"/>
+        <location filename="../mxwelcome.cpp" line="99"/>
         <source>License</source>
         <translation>Notkunarleyfi</translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="100"/>
+        <location filename="../mxwelcome.cpp" line="100"/>
         <source>Changelog</source>
         <translation>Breytingaskrá</translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="101"/>
+        <location filename="../mxwelcome.cpp" line="101"/>
         <source>Cancel</source>
         <translation>Hætta við</translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="117"/>
+        <location filename="../mxwelcome.cpp" line="117"/>
         <source>&amp;Close</source>
         <translation>&amp;Loka</translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="190"/>
+        <location filename="../mxwelcome.cpp" line="190"/>
         <source>Live Login Info</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="191"/>
+        <location filename="../mxwelcome.cpp" line="191"/>
         <source>Default username:  &lt;b&gt;demo&lt;/b&gt; &lt;/p&gt; &lt;p align=&quot;center&quot;&gt;Default demo password:  &lt;b&gt;demo&lt;/b&gt;</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="192"/>
+        <location filename="../mxwelcome.cpp" line="192"/>
         <source>&lt;b&gt;Root&lt;/b&gt; Password:  &lt;b&gt;root&lt;/b&gt;&lt;/p&gt; </source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
