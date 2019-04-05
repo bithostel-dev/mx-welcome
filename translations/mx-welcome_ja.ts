90f72e13499e497bf07827513e4f0d98275e27dd
Adrian <adrian@mxlinux.org>
2019-04-05 12:05:32 -0400

    update translaptions

--- translations/mx-welcome_ja.ts
+++ translations/mx-welcome_ja.ts
@@ -1,8 +1,10 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="ja" version="2.1">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja">
 <context>
     <name>QApplication</name>
     <message>
-        <location filename="main.cpp" line="55"/>
+        <location filename="../main.cpp" line="55"/>
         <source>You must run this program as normal user.</source>
         <translation>このプログラムは通常ユーザーで実行して下さい。</translation>
     </message>
@@ -10,150 +12,150 @@
 <context>
     <name>mxwelcome</name>
     <message>
-        <location filename="mxwelcome.ui" line="14"/>
-        <location filename="mxwelcome.cpp" line="54"/>
-        <location filename="mxwelcome.cpp" line="99"/>
-        <location filename="mxwelcome.cpp" line="111"/>
+        <location filename="../mxwelcome.ui" line="14"/>
+        <location filename="../mxwelcome.cpp" line="54"/>
+        <location filename="../mxwelcome.cpp" line="99"/>
+        <location filename="../mxwelcome.cpp" line="111"/>
         <source>MX Welcome</source>
         <translation>MX Welcome</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="70"/>
+        <location filename="../mxwelcome.ui" line="70"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:12pt; font-weight:600;&quot;&gt;Welcome to MX Linux, a fast, friendly, and stable OS loaded with amazing tools and supported by a great community. &lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:12pt; font-weight:600;&quot;&gt;MX Linux へようこそ。MX Linux は高速でフレンドリー、安定した OS は素晴らしいツールを使って読み込まれ、偉大なコミュニティでサポートされています。 &lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="113"/>
+        <location filename="../mxwelcome.ui" line="113"/>
         <source>Popular Apps</source>
         <translation>一般的なアプリ</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="203"/>
+        <location filename="../mxwelcome.ui" line="203"/>
         <source>Forums</source>
         <translation>フォーラム</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="280"/>
+        <location filename="../mxwelcome.ui" line="280"/>
         <source>Videos</source>
         <translation>ビデオ</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="377"/>
+        <location filename="../mxwelcome.ui" line="377"/>
         <source>Contribute</source>
         <translation>支援・貢献</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="422"/>
+        <location filename="../mxwelcome.ui" line="422"/>
         <source>Tweak (Panel, etc...)</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="486"/>
+        <location filename="../mxwelcome.ui" line="486"/>
         <source>Tools</source>
         <translation>ツール</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="512"/>
+        <location filename="../mxwelcome.ui" line="512"/>
         <source>Codecs</source>
         <translation>Codecs</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="544"/>
+        <location filename="../mxwelcome.ui" line="544"/>
         <source>Wiki</source>
         <translation>Wiki</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="576"/>
+        <location filename="../mxwelcome.ui" line="576"/>
         <source>Users Manual</source>
         <translation>ユーザーマニュアル</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="608"/>
+        <location filename="../mxwelcome.ui" line="608"/>
         <source>FAQ</source>
         <translation>FAQ</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="641"/>
+        <location filename="../mxwelcome.ui" line="641"/>
         <source>Show this dialog at start up</source>
         <translation>起動時にこのダイヤログを表示する</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="706"/>
+        <location filename="../mxwelcome.ui" line="706"/>
         <source>About this application</source>
         <translation>このアプリケーションについて</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="709"/>
+        <location filename="../mxwelcome.ui" line="709"/>
         <source>About...</source>
         <translation>About...</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="716"/>
+        <location filename="../mxwelcome.ui" line="716"/>
         <source>Alt+B</source>
         <translation>Alt+B</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="770"/>
+        <location filename="../mxwelcome.ui" line="770"/>
         <source>Quit application</source>
         <translation>アプリケーションの終了</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="773"/>
+        <location filename="../mxwelcome.ui" line="773"/>
         <source>Close</source>
         <translation>閉じる</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="780"/>
+        <location filename="../mxwelcome.ui" line="780"/>
         <source>Alt+N</source>
         <translation>Alt+N</translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="57"/>
+        <location filename="../mxwelcome.cpp" line="57"/>
         <source>Default username:  &lt;b&gt;demo&lt;/b&gt; &lt;/p&gt; &lt;p align=&quot;center&quot;&gt;Default demo password:  &lt;b&gt;demo&lt;/b&gt;</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="58"/>
+        <location filename="../mxwelcome.cpp" line="58"/>
         <source>&lt;b&gt;Root&lt;/b&gt; Password:  &lt;b&gt;root&lt;/b&gt;&lt;/p&gt; </source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="98"/>
+        <location filename="../mxwelcome.cpp" line="98"/>
         <source>About MX Welcome</source>
         <translation>MX Welcome について</translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="99"/>
+        <location filename="../mxwelcome.cpp" line="99"/>
         <source>Version: </source>
         <translation>Version: </translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="100"/>
+        <location filename="../mxwelcome.cpp" line="100"/>
         <source>Program for displaying a welcome screen in MX Linux</source>
         <translation>MX Linux の Welcome 画面を表示するプログラム</translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="102"/>
+        <location filename="../mxwelcome.cpp" line="102"/>
         <source>Copyright (c) MX Linux</source>
         <translation>Copyright (c) MX Linux</translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="103"/>
+        <location filename="../mxwelcome.cpp" line="103"/>
         <source>License</source>
         <translation>ライセンス</translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="104"/>
+        <location filename="../mxwelcome.cpp" line="104"/>
         <source>Changelog</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="105"/>
+        <location filename="../mxwelcome.cpp" line="105"/>
         <source>Cancel</source>
         <translation>キャンセル</translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="121"/>
+        <location filename="../mxwelcome.cpp" line="121"/>
         <source>&amp;Close</source>
         <translation>閉じる(&amp;C)</translation>
     </message>
--- translations/mx-welcome_lt.ts
+++ translations/mx-welcome_lt.ts
@@ -1,8 +1,10 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="lt" version="2.1">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lt">
 <context>
     <name>QApplication</name>
     <message>
-        <location filename="main.cpp" line="55"/>
+        <location filename="../main.cpp" line="55"/>
         <source>You must run this program as normal user.</source>
         <translation>Privalote paleisti šią programą kaip normalus naudotojas.</translation>
     </message>
@@ -10,150 +12,150 @@
 <context>
     <name>mxwelcome</name>
     <message>
-        <location filename="mxwelcome.ui" line="14"/>
-        <location filename="mxwelcome.cpp" line="54"/>
-        <location filename="mxwelcome.cpp" line="99"/>
-        <location filename="mxwelcome.cpp" line="111"/>
+        <location filename="../mxwelcome.ui" line="14"/>
+        <location filename="../mxwelcome.cpp" line="54"/>
+        <location filename="../mxwelcome.cpp" line="99"/>
+        <location filename="../mxwelcome.cpp" line="111"/>
         <source>MX Welcome</source>
         <translation>MX pasveikinimas</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="70"/>
+        <location filename="../mxwelcome.ui" line="70"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:12pt; font-weight:600;&quot;&gt;Welcome to MX Linux, a fast, friendly, and stable OS loaded with amazing tools and supported by a great community. &lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:12pt; font-weight:600;&quot;&gt;Sveiki atvykę į MX Linux, greitą, draugišką ir stabilią OS, pripildytą puikių įrankių ir palaikomą nuostabios bendruomenės. &lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="113"/>
+        <location filename="../mxwelcome.ui" line="113"/>
         <source>Popular Apps</source>
         <translation>Populiarios programos</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="203"/>
+        <location filename="../mxwelcome.ui" line="203"/>
         <source>Forums</source>
         <translation>Forumai</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="280"/>
+        <location filename="../mxwelcome.ui" line="280"/>
         <source>Videos</source>
         <translation>Vaizdo įrašai</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="377"/>
+        <location filename="../mxwelcome.ui" line="377"/>
         <source>Contribute</source>
         <translation>Talkinti</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="422"/>
+        <location filename="../mxwelcome.ui" line="422"/>
         <source>Tweak (Panel, etc...)</source>
         <translation>Patobulinimai (Skydelis ir t.t. ...)</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="486"/>
+        <location filename="../mxwelcome.ui" line="486"/>
         <source>Tools</source>
         <translation>Įrankiai</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="512"/>
+        <location filename="../mxwelcome.ui" line="512"/>
         <source>Codecs</source>
         <translation>Kodekai</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="544"/>
+        <location filename="../mxwelcome.ui" line="544"/>
         <source>Wiki</source>
         <translation>Vikis</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="576"/>
+        <location filename="../mxwelcome.ui" line="576"/>
         <source>Users Manual</source>
         <translation>Naudotojo vadovas</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="608"/>
+        <location filename="../mxwelcome.ui" line="608"/>
         <source>FAQ</source>
         <translation>DUK</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="641"/>
+        <location filename="../mxwelcome.ui" line="641"/>
         <source>Show this dialog at start up</source>
         <translation>Rodyti šį dialogą paleidimo metu</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="706"/>
+        <location filename="../mxwelcome.ui" line="706"/>
         <source>About this application</source>
         <translation>Apie šią programą</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="709"/>
+        <location filename="../mxwelcome.ui" line="709"/>
         <source>About...</source>
         <translation>Apie...</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="716"/>
+        <location filename="../mxwelcome.ui" line="716"/>
         <source>Alt+B</source>
         <translation>Alt+B</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="770"/>
+        <location filename="../mxwelcome.ui" line="770"/>
         <source>Quit application</source>
         <translation>Išeiti iš programos</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="773"/>
+        <location filename="../mxwelcome.ui" line="773"/>
         <source>Close</source>
         <translation>Užverti</translation>
     </message>
     <message>
-        <location filename="mxwelcome.ui" line="780"/>
+        <location filename="../mxwelcome.ui" line="780"/>
         <source>Alt+N</source>
         <translation>Alt+N</translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="57"/>
+        <location filename="../mxwelcome.cpp" line="57"/>
         <source>Default username:  &lt;b&gt;demo&lt;/b&gt; &lt;/p&gt; &lt;p align=&quot;center&quot;&gt;Default demo password:  &lt;b&gt;demo&lt;/b&gt;</source>
         <translation>Numatytasis naudotojo vardas:  &lt;b&gt;demo&lt;/b&gt; &lt;/p&gt; &lt;p align=&quot;center&quot;&gt;Numatytasis demo slaptažodis:  &lt;b&gt;demo&lt;/b&gt;</translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="58"/>
+        <location filename="../mxwelcome.cpp" line="58"/>
         <source>&lt;b&gt;Root&lt;/b&gt; Password:  &lt;b&gt;root&lt;/b&gt;&lt;/p&gt; </source>
         <translation>&lt;b&gt;Root&lt;/b&gt; Slaptažodis:  &lt;b&gt;root&lt;/b&gt;&lt;/p&gt; </translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="98"/>
+        <location filename="../mxwelcome.cpp" line="98"/>
         <source>About MX Welcome</source>
         <translation>Apie MX pasveikinimas</translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="99"/>
+        <location filename="../mxwelcome.cpp" line="99"/>
         <source>Version: </source>
         <translation>Versija: </translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="100"/>
+        <location filename="../mxwelcome.cpp" line="100"/>
         <source>Program for displaying a welcome screen in MX Linux</source>
         <translation>Programa, skirta rodyti pasisveikinimo langą MX Linux</translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="102"/>
+        <location filename="../mxwelcome.cpp" line="102"/>
         <source>Copyright (c) MX Linux</source>
         <translation>Autorių teisės (c) MX Linux</translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="103"/>
+        <location filename="../mxwelcome.cpp" line="103"/>
         <source>License</source>
         <translation>Licencija</translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="104"/>
+        <location filename="../mxwelcome.cpp" line="104"/>
         <source>Changelog</source>
         <translation>Keitinių žurnalas</translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="105"/>
+        <location filename="../mxwelcome.cpp" line="105"/>
         <source>Cancel</source>
         <translation>Atsisakyti</translation>
     </message>
     <message>
-        <location filename="mxwelcome.cpp" line="121"/>
+        <location filename="../mxwelcome.cpp" line="121"/>
         <source>&amp;Close</source>
         <translation>&amp;Užverti</translation>
     </message>
